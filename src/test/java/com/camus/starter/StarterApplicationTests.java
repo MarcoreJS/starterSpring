78d13846d7d86353cda3a24b80dfb89deb46247b
Marco Mercado <marcore.dmg@gmail.com>
2020-11-14 08:38:48 -0600

    spring starter

---
+++ src/test/java/com/camus/starter/StarterApplicationTests.java
@@ -0,0 +1,10 @@
+package com.camus.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+
+}
